8d9f4b5bafb0b1a0365e532c866f2c5d9bd44ca5
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-05-13 11:28:50 +0200

    basic front end set up

---
+++ client/src/components/Furnace.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Furnace extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react";
+
+export default function NavBar() {
+  return (
+    <div className="Nav">
+    <img src="/images/logo.jpg" alt="logo" />
+      <ul>
+        <li>home</li>
+        <li>FURNACE</li>
+      </ul>
+    </div>
+  );
+}
