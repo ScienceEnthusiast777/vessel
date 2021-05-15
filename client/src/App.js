a809c4e5961153e0922681e080dcc4d25e43156f
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-05-15 17:47:35 +0200

    add download button in new componenet Offerings

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import Furnace from './components/Furnace'
-
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Furnace from "./components/Furnace";
+import Offerings from "./components/Offerings";
 
 function App() {
   return (
     <div className="App">
-        <NavBar/>
-     <Route exact path='/furnace' component={Furnace} />
+      <NavBar />
+      <Switch>
+      <Route exact path="/offerings" component={Offerings} />
+        <Route exact path="/furnace" component={Furnace} />
+       
+      </Switch>
     </div>
   );
 }
--- client/src/components/Offerings.js
+++ client/src/components/Offerings.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class Offerings extends Component {
 
   state = {
     offerings: []
   }
+
+  Download = (e) => {
+    axios({url:'/api/furnace/', method: 'GET', responseType: 'blb'})
+        .then((response) => console.log('processed the file: ',response.data))
+        .catch((err)=>console.log(err));
+  }
+  
   render() {
     return (
       <div>
-        
+        <button onClick={this.Download}>Download</button>
       </div>
     )
   }
