0785e35cba06858a22382248edce5c0ca4df9e2c
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-05-17 19:59:24 +0200

    add restricted path component

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,51 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import React from 'react';
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Furnace from "./components/Furnace";
 import Offerings from "./components/Offerings";
 import SignUp from "./components/SignUp";
 import LogIn from "./components/LogIn";
+import Restricted from "./components/Restricted";
 
-
-class App extends React.Component{
-
+class App extends React.Component {
   state = {
-    user: this.props.user
-  }
+    user: this.props.user,
+  };
 
-  setUser = user =>{
+  setUser = (user) => {
     this.setState({
-      user : user
-    })
-  }
+      user: user,
+    });
+  };
 
-  render(){
-    console.log(this.state.user)
+  render() {
+    console.log(this.state.user);
     return (
       <div className="App">
-        <NavBar user={this.state.user} setUser={this.setUser
-        }/>
+        <NavBar user={this.state.user} setUser={this.setUser} />
         <Switch>
-          <Route exact path="/offerings" component={Offerings} />
-          <Route exact path="/furnace" component={Furnace} />
-          <Route exact path="/signup" 
-          render={props=> <SignUp setUser={this.setUser}{...props}/>} />
-          <Route exact path="/login" 
-          render={props=> <LogIn setUser={this.setUser}{...props}/>}/>
+          <Restricted
+            path="/offerings"
+            user={this.state.user}
+            component={Offerings}
+          />
+          <Restricted
+            path="/furnace"
+            user={this.state.user}
+            component={Furnace}
+          />
+          <Route
+            exact
+            path="/signup"
+            render={(props) => <SignUp setUser={this.setUser} {...props} />}
+          />
+          <Route
+            exact
+            path="/login"
+            render={(props) => <LogIn setUser={this.setUser} {...props} />}
+          />
         </Switch>
       </div>
     );
