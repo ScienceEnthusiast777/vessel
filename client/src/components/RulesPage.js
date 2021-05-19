9b35ea23e53178948a215255ab9fd5b0edce5f00
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-05-19 15:48:56 +0200

    add extensions to rules list page

--- client/src/components/RulesPage.js
+++ client/src/components/RulesPage.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 export default function RulesPage(props) {
+  
   return (
     <div>
       {props.rules.map(rule=>{
         return (
-          <div key={rule._id}>
-          <h1><Link to={`/rule/${rule._id}`} className="RuleLink">{rule.name}</Link></h1>
+          <div className="RuleLink" key={rule._id}>
+          <h1><Link to={`/rule/${rule._id}`} >{rule.name}</Link></h1>
+          {rule.extensions.length>0?<p>This ruleset has been extended: {rule.extensions.length} time(s)</p>:<></>}
+          
           <p>created by : {rule.createdBy.username}</p>
           </div>
         )
