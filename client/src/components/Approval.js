b5162032ac6b5e9e8a6cde425527ce4b0016de91
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-05-19 20:43:46 +0200

    add backend functionality for users to mark extensions as approved

--- client/src/components/Approval.js
+++ client/src/components/Approval.js
@@ -3,25 +3,29 @@ import axios from 'axios';
 
 export default class Approval extends Component {
   state = {
+    // index: NaN,
     extension: null,
     ruleId: "",
   };
 
   componentDidMount() {
     this.setState({
+      index: this.props.index,
       extension: this.props.extension,
       ruleId: this.props.rule
     });
   }
 
   handleApproval = () =>{
-    const {approved} = this.state.extension; 
-    axios.put(`/api/rules/approve/${this.state.ruleId}`, {approved} )
+    const _id = this.state.extension._id;
+    console.log(_id)
+    // const index = this.state.index; 
+    axios.put(`/api/rules/approve/${this.state.ruleId}`, {_id} )
     .then((response) => {
-              this.props.history.push("/ruleslist");
+      this.props.history.push("/ruleslist");
     });
   }
-  
+
   render() {
     return <div>
       {this.state.extension?<><button onClick={this.handleApproval}>approve {this.state.extension.extendedBy.username}'s extension of your rule system</button></>:<></>}
