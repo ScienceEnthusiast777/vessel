832bc80eec150427a6ba27079ae0dcca6185b0a1
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-07-06 12:55:12 +0200

    add basic styling for nav bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,51 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { logout } from '../services/auth';
+import { logout } from "../services/auth";
 
 export default function NavBar(props) {
-
-  const logoutHandler = () =>{
-    logout().then(()=>{
+  const logoutHandler = () => {
+    logout().then(() => {
       props.setUser(null);
-    })
-  }
+    });
+  };
 
   return (
-    <div className="bg-white">
-    <div className="Display">
-    {props.user?<p>logged in as: {props.user.username}</p>:<></>}
-      <img src="/images/logo.jpg" alt="logo" />
-    </div>
+    <div className="flex flex-row justify-around pt-20">
+      <div className="Display">
+        <img className="h-40" src="/images/logo.jpg" alt="logo" />
+      </div>
       <ul className="NavButtons">
-        <li>
+        <li className="cursor-pointer border border-4 hover:border-black m-2">
           <Link to="/">HOME</Link>
         </li>
         {!props.user ? (
           <>
-            <li>
+            <li className="cursor-pointer border border-4 hover:border-black m-2">
               <Link to="/signup">SIGN UP</Link>
             </li>
-            <li>
+            <li className="cursor-pointer border border-4 hover:border-black m-2">
               <Link to="/login">LOG IN</Link>
             </li>
           </>
         ) : (
           <>
-            <li>
-              <Link to='/' onClick={()=>logoutHandler()}>LOG OUT</Link>
+            <li className="cursor-pointer border border-4 hover:border-black m-2">
+              <Link to="/" onClick={() => logoutHandler()}>
+                LOG OUT
+              </Link>
             </li>
-            <li>
+            <li className="cursor-pointer border border-4 hover:border-black m-2">
               <Link to="/furnace">FURNACE</Link>
             </li>
-            <li>
+            <li className="cursor-pointer border border-4 hover:border-black m-2">
               <Link to="/offerings">OFFERINGS</Link>
             </li>
-            <li>
+            <li className="cursor-pointer border border-4 hover:border-black m-2">
               <Link to="/ruleslist">RULE SYSTEMS</Link>
             </li>
-            <li>
+            <li className="cursor-pointer border border-4 hover:border-black m-2">
               <Link to="/rulescreate">CREATE RULES</Link>
             </li>
+            {props.user ? <p>logged in as: {props.user.username}</p> : <></>}
           </>
         )}
       </ul>
